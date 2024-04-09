d9505f288d8434ac118b4e5d43df5709f48fa90c
Alexandre Paiva <alexandre.paiva@loadsmart.com>
2024-04-09 13:11:06 -0300

    Prev and Next

--- src/cv-generator.js
+++ src/cv-generator.js
@@ -1,7 +1,8 @@
 
 
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { FaQuestionCircle } from "react-icons/fa";
+import { GrCaretNext, GrCaretPrevious } from "react-icons/gr";
 
 const toHTML = (text) => {
     // Regular expression to match Markdown links
@@ -54,14 +55,48 @@ function Experience({ experience }) {
 }
 
 function ExperienceList({ experiences }) {
+    const [currentPage, setCurrentPage] = useState(1);
+    const itemsPerPage = 3;
+    const prevRef = useRef(null);
+    const nextRef = useRef(null);
+
+    // Calculate start and end index of items to display
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+
+    const allItems = experiences;
+
+    // Get items for the current page
+    const currentItems = allItems.slice(startIndex, endIndex);
+
+    // Handler for next page click
+    const nextPage = () => {
+        setCurrentPage(currentPage + 1);
+    };
+
+    // Handler for previous page click
+    const prevPage = () => {
+        setCurrentPage(currentPage - 1);
+    };
+
     return (
         <div className="experiences">
-            {experiences.map((experience) => (
+            {currentItems.map((experience) => (
                 <Experience experience={experience} />
             ))}
+            <div className='pagination'>
+                {currentPage > 1 && (
+                    <a href="#" className='prev' ref={prevRef} onClick={prevPage}><GrCaretPrevious/></a>
+                )}
+                {endIndex < allItems.length && (
+                    <a href="#" className='next' ref={nextRef} onClick={nextPage}><GrCaretNext/></a>
+                )}
+            </div>
         </div>
     );
 }
+
+
 function DictToHTML({ jsonString }) {
 
     let data = null;
