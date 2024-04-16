ed3be54fb0eff334f6428bfe0c98a27d1a1c420a
Alexandre Paiva <alexandre.paiva@loadsmart.com>
2024-04-16 09:50:10 -0300

    Separate CSS for Hint

--- src/components/Hint.js
+++ src/components/Hint.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import { FaQuestionCircle } from "react-icons/fa";
 import { toHTML } from '../parser/experiences';
 
+import './Hint.css';
+
 export function Hint({ hint }) {
     const [isHovered, setIsHovered] = useState(false);
     const [position, setPosition] = useState({ x: 0, y: 0 });
@@ -20,13 +22,18 @@ export function Hint({ hint }) {
         setIsHovered(false);
     };
 
-    return (hint && (<>
-        <div className="hint">
-            <div onMouseMove={updatePosition}>
-                <div className="icon" onMouseEnter={mouseEnter} onMouseLeave={mouseLeave}><FaQuestionCircle /></div>
+    return (hint &&
+        (<>
+            <div className="hint">
+                <div onMouseMove={updatePosition}>
+                    <div className="icon" onMouseEnter={mouseEnter} onMouseLeave={mouseLeave}><FaQuestionCircle /></div>
+                </div>
             </div>
-        </div>
-        {isHovered && ReactDOM.createPortal(<div className="box" style={{ top: position.y, left: position.x }} onMouseEnter={mouseEnter} onMouseLeave={mouseLeave} dangerouslySetInnerHTML={{ __html: toHTML(hint) }} />, document.body)}
-    </>
-    ));
+            {isHovered && ReactDOM.createPortal(
+                <div className="box" style={{ top: position.y, left: position.x }}
+                    onMouseEnter={mouseEnter} onMouseLeave={mouseLeave}
+                    dangerouslySetInnerHTML={{ __html: toHTML(hint) }} />,
+                document.body)}
+        </>
+        ));
 }
