4118c126113922104fb2e4331547fd72de8dd227
Alexandre Paiva <alexandre.paiva@loadsmart.com>
2024-04-22 09:01:11 -0300

    Hiding Experience when tag is not present

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -2,20 +2,22 @@ import React, { useState } from 'react';
 import { Hint, HintIcon } from './Hint';
 import { Roles } from './Roles';
 import { Paragraphs } from './Paragraphs';
+import { Visible } from './Visible';
 
 export function Experience({ experience }) {
     const [isVisible, setIsVisible] = useState(false);
+    const contents = <div className="experience">
+        <Roles roles={experience.roles} />
+        <div className="company-period">
+            <div className="column"><div className="company">{experience.company.name}</div></div>
+            <div className="column"><div className="period">{experience.start && `${experience.start} - ${experience.end}`}</div></div>
+            <div className="column"><HintIcon isVisible={isVisible} setIsVisible={setIsVisible} /></div>
+        </div>
+        <Hint hint={experience.company.hint} isVisible={isVisible} />
+        <Paragraphs paragraphs={experience.paragraphs} className={"paragraphs"} />
+    </div>
 
     return (
-        <div className="experience">
-            <Roles roles={experience.roles} />
-            <div className="company-period">
-                <div className="column"><div className="company">{experience.company.name}</div></div>
-                <div className="column"><div className="period">{experience.start && `${experience.start} - ${experience.end}`}</div></div>
-                <div className="column"><HintIcon isVisible={isVisible} setIsVisible={setIsVisible} /></div>
-            </div>
-            <Hint hint={experience.company.hint} isVisible={isVisible} />
-            <Paragraphs paragraphs={experience.paragraphs} className={"paragraphs"}/>
-        </div>
+        <Visible tags={experience.tags} contents={contents} />
     );
 }
--- src/components/Visible.js
+++ src/components/Visible.js
@@ -6,7 +6,7 @@ export function Visible({ tags, contents }) {
     const { globalContext } = useGlobalContext();
 
     const visible = tags ? tags.some(tag => globalContext["labelSelected"][tag.toLowerCase()]) : true;
-    console.log("Visible? ", visible, tags)
+
     return <div>{visible && contents}</div>;
 }
 
